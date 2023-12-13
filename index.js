ba32557c6a58f4dde4a45a7ac8a7f23c7bae1244
Edona Lleshi <edona.lleshi95@gmail.com>
2023-12-13 15:57:17 +0100

    add js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // phew… not a lot going on here. Please add some code!
+const bookmarkButton = document.querySelector('.bookmark');
+bookmarkButton.addEventListener('click', function () {
+  
+  this.classList.toggle('active');
+});
+
+const answerButton = document.querySelector('.card__button-answer');
+answerButton.addEventListener('click', function () {
+ 
+  const answerParagraph = document.querySelector('.card__answer');
+  answerParagraph.classList.toggle('hidden');
+
+
+  const buttonText = this.innerText === 'Show answer' ? 'Hide answer' : 'Show answer';
+  this.innerText = buttonText;
+});
