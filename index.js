beb6909b7663939cfe534d8e8b9a20d69ebb20b9
Edona Lleshi <edona.lleshi95@gmail.com>
2023-12-14 11:32:17 +0100

    answer-button

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 // phew… not a lot going on here. Please add some code!
-const bookmarkButton = document.querySelector('.bookmark');
-bookmarkButton.addEventListener('click', function () {
-  
-  this.classList.toggle('active');
+//const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+
+//bookmarkButton.addEventListener("click", () => {
+  //bookmarkButton.classList.toggle("bookmark--active");
+//});
+
+document.querySelectorAll('.bookmark').forEach(bookmarkButton => {
+  bookmarkButton.addEventListener('click', function () {
+    this.classList.toggle('active');
+    const icon = this.querySelector('.bookmark__icon');
+    icon.style.fill = this.classList.contains('active') ? 'black' : 'white';
+  });
 });
 
-const answerButton = document.querySelector('.card__button-answer');
-answerButton.addEventListener('click', function () {
- 
-  const answerParagraph = document.querySelector('.card__answer');
-  answerParagraph.classList.toggle('hidden');
-
-
-  const buttonText = this.innerText === 'Show answer' ? 'Hide answer' : 'Show answer';
-  this.innerText = buttonText;
+document.querySelectorAll('.card__button-answer').forEach(answerButton => {
+  answerButton.addEventListener('click', function () {
+    const answerParagraph = this.closest('.card').querySelector('.card__answer');
+    answerParagraph.classList.toggle('hidden');
+    this.innerText = answerParagraph.classList.contains('hidden') ? 'Show answer' : 'Hide answer';
+  });
 });
